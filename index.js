5fc04445446c7e1bc37a7bac122428db4088b9c8
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-08 23:11:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+var a = "1"
+var b = "80"
+
+function add(a, b) {
+  return(a + b)
+}
+
+var a = "60"
+var b = "40"
+
+function subtract(a, b) {
+  return(a - b)
+}
+
+var a = "2"
+var b = "3.4"
+
+function multiply(a, b) {
+  return(a * b)
+}
+
+var a = "5.0"
+var b = "2.5"
+
+function divide(a, b) {
+  return(a / b)
+}
+
+var n = 10
+
+function inc(n) {
+  return(n += 1)
+}
+
+function dec(n) {
+  return(n -= 1)
+}
+
+function makeInt(a){
+  parseFloat(a, 10)
+  return parseInt(a, 10)
+}
